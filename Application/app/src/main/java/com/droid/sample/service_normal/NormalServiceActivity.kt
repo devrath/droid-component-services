8e0b00ba633c6aaf822d21cca9d375d67faeabf9
Devrath <devrath@mplgaming.com>
2022-09-09 21:02:37 +0530

    reformat

--- Application/app/src/main/java/com/droid/sample/service_normal/NormalServiceActivity.kt
+++ Application/app/src/main/java/com/droid/sample/service_normal/NormalServiceActivity.kt
@@ -3,10 +3,8 @@ package com.droid.sample.service_normal
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.droid.sample.databinding.ActivityMainBinding
 import com.droid.sample.databinding.ActivityServiceNormalBinding
 import com.droid.sample.service_normal.services.NormalDownloadService
-import com.droid.sample.utils.extensions.intent.openActivity
 import com.droid.sample.utils.extensions.toast.toast
 
 class NormalServiceActivity : AppCompatActivity() {
@@ -25,10 +23,18 @@ class NormalServiceActivity : AppCompatActivity() {
             }
 
             checkServiceId.setOnClickListener {
-                if(NormalDownloadService.isCurrentServiceRunning()){
-                    toast(msg = "Service is running",isShort = true, context = this@NormalServiceActivity)
-                }else{
-                    toast(msg = "Service is not running",isShort = true, context = this@NormalServiceActivity)
+                if (NormalDownloadService.isCurrentServiceRunning()) {
+                    toast(
+                        msg = "Service is running",
+                        isShort = true,
+                        context = this@NormalServiceActivity
+                    )
+                } else {
+                    toast(
+                        msg = "Service is not running",
+                        isShort = true,
+                        context = this@NormalServiceActivity
+                    )
                 }
             }
 
@@ -36,10 +42,6 @@ class NormalServiceActivity : AppCompatActivity() {
                 NormalDownloadService.stopService()
             }
 
-
         }
     }
-
-
-
 }
